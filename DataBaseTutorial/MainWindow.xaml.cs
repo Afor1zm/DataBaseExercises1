c1cd2c36bc069f573f6c659b798b4a91034bf41d
Afor1zm <aforizms@mail.ru>
2020-05-22 16:28:47 +0300

    First Commit. Создал список из 5ти продуктов, которые хранятся на складе для торговли. Каждому продукту присвоено 5 свойств (ID, Название, Масса, СтухИлиНет, Дата покупки на скалд)

---
+++ DataBaseTutorial/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace DataBaseTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        List<Product> myProducts;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }    
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            myProducts = new List<Product>();
+            myProducts.Add(new Product("Onion", 01, 0.5, true,"01/02/2020 07:32:17"));
+            myProducts.Add(new Product("Salat", 02, 1, true, "01/02/2020 07:32:17"));
+            myProducts.Add(new Product("Pickle", 03, 2.5, true, "01/02/2020 07:32:17"));
+            myProducts.Add(new Product("Potato", 04, 8.56, true, "01/02/2020 07:32:17"));
+            myProducts.Add(new Product("Tomato", 05, 0.05, false, "01/02/2020 07:32:17"));
+
+            ProductList.ItemsSource = myProducts;
+            
+        }
+    }
+}
---
+++ DataBaseTutorial/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBaseTutorial
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+
+        public int ProductId { get; set; }
+        public double ProductWeight {get;set;}
+        public bool ProductAddled { get; set; }
+        public DateTime ProductBuyedDate { get; set; } 
+
+        public Product(string productName, int productID, double productWeight, bool productAddled, string productBuyed)
+        {
+            
+            ProductName = productName;
+            ProductId = productID;
+            ProductWeight = productWeight;
+            ProductAddled = productAddled;
+            ProductBuyedDate = Convert.ToDateTime(productBuyed);
+            ProductBuyedDate.ToShortDateString();
+        }
+    }
+}
